4301f3b839d48ef56471fac7beefac8028a8e139
whatcoloris <brian.mcdorkle@gmail.com>
2023-05-09 19:59:36 -0400

    working locally, need to push to heroku

--- README.md
+++ README.md
@@ -15,4 +15,58 @@ To use the batch retrieval tool, create a CSV with the minimum required fields a
 
 The output can be downloaded as a CSV or viewed on the page.
 
-##
+## appendix
+
+### Musicbrainz API with musicbrainzngs
+
+More info at: 
+[https://python-musicbrainzngs.readthedocs.io/en/latest/api/](https://python-musicbrainzngs.readthedocs.io/en/latest/api/)
+
+For this application, since we are trying to get as many releases as possible but not things that are "like" the pertinent release in the collection, we must be very specific with our query.
+
+Below are some very helpful search functions in `musicbrainzngs` which we can use in Python to query the Musicbrainz database.
+
+`musicbrainzngs.search_recordings(query='', limit=None, offset=None, strict=False, **fields)`
+*Search for recordings and return a dict with a ‘recording-list’ key.*
+
+>Available search fields: alias, arid, artist, artistname, comment, country, creditname, date, dur, format, isrc, number, position, primarytype, qdur, recording, recordingaccent, reid, release, rgid, rid, secondarytype, status, tag, tid, tnum, tracks, tracksrelease, type, video
+
+`musicbrainzngs.search_release_groups(query='', limit=None, offset=None, strict=False, **fields)`
+*Search for release groups and return a dict with a ‘release-group-list’ key.*
+
+>Available search fields: alias, arid, artist, artistname, comment, creditname, primarytype, reid, release, releasegroup, releasegroupaccent, releases, rgid, secondarytype, status, tag, type
+
+`musicbrainzngs.search_releases(query='', limit=None, offset=None, strict=False, **fields)`
+*Search for recordings and return a dict with a ‘recording-list’ key.*
+
+>Available search fields: alias, arid, artist, artistname, asin, barcode, catno, comment, country, creditname, date, discids, discidsmedium, format, label, laid, lang, mediums, primarytype, quality, reid, release, releaseaccent, rgid, script, secondarytype, status, tag, tracks, tracksmedium, type
+
+After some experimentation, it seems that `search_release_groups` is the best function for our purposes here.
+
+Since we do not have much control over what CSVs come in, we should try our best to match each column of the given CSV with the available fields for `search_release_groups`.
+
+You can use `search_release_groups` without specifying the parameters, by just submitting a query: `musicbrainzngs.search_release_group('"robert ashley" AND "yellow man with heart with wings"')`.
+
+So for example, we could search based on just the artist and title, and then search everything else as optional, or search specific terms first, then if there are no results, search without double quotes/exact modifiers.
+
+More in-depth information about Queries is available on the [package description website about Lucene Search Syntax](https://lucene.apache.org/core/7_7_2/queryparser/org/apache/lucene/queryparser/classic/package-summary.html#package.description).
+
+### Discogs API
+
+More info at: [https://python3-discogs-client.readthedocs.io/en/latest/fetching_data.html](https://python3-discogs-client.readthedocs.io/en/latest/fetching_data.html)
+
+We can use the `search` function like so:
+`d.search(release_title, type='release')`
+
+This returns a `release` object, which has the following parameters:
+- artists
+- formats
+- genres
+- images
+- tracklist
+- title
+
+As an example, to print the primary image associated with a release, one would:
+
+`results = d.search(ex_title, type='release')`
+`print(d.results.images[0])`
--- app.py
+++ app.py
@@ -1,28 +1,19 @@
+# flask imports
 from flask import Flask, render_template, request, session
-
+# misc system imports
 import os
 import urllib3
+# music DB API imports
+import musicbrainzngs
 import discogs_client
+# CSV imports
 import pandas as pd
 from werkzeug.utils import secure_filename
+# dotenv import
+from dotenv import load_dotenv
 
+# FLASK CONFIG
 app = Flask(__name__)
-
-#user_token = os.environ.get("USER_TOKEN")
-#http = urllib3.PoolManager()
-#r = http.request('GET', 'https://musicbrainz.org/ws/2/') # grab music information from musicbrainz
-
-
-#d = discogs_client.Client('ExampleApplication/0.1', user_token=user_token)
-
-# @app.route("/")
-# def find_result():
-#     results = d.search('SHAKER LOOPS', type='release')
-
-#     for release in results:
-#         return release.title
-
-#*** Flask configuration
 
 # Define folder to save uploaded files to process further
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
@@ -35,11 +26,23 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Define secret key to enable session
-app.secret_key = 'This is your secret key to utilize session in Flask'
- 
+load_dotenv()
+
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# musicbrainz User Agent Setup
+musicbrainzngs.set_useragent(
+    "batch-api-retrieval",
+    "0.1",
+    "https://github.com/computational-rarity-team/batch-api-retrieval/",
+)
+d = discogs_client.Client('ExampleApplication/0.1', user_token=app.secret_key)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
  
 @app.route('/',  methods=("POST", "GET"))
 def uploadFile():
@@ -58,6 +61,7 @@ def uploadFile():
  
         return render_template('success.html')
  
+
 @app.route('/show_data')
 def showData():
     # Retrieving uploaded file path from session
@@ -69,7 +73,74 @@ def showData():
     # pandas dataframe to html table flask
     uploaded_df_html = uploaded_df.to_html()
     return render_template('show_data.html', data_var = uploaded_df_html)
+
+
+@app.route('/check_fields')
+def checkFields():
+    # Retrieving uploaded file path from session
+    data_file_path = session.get('uploaded_data_file_path', None)
  
+    # read csv file in python flask (reading uploaded csv file from uploaded server location)
+    uploaded_df = pd.read_csv(data_file_path)
+
+    # first check fields of CSV with search params of MB and Discogs
+    # !!!! for this version let's just do artist and title/release
+    return render_template('check_fields.html', column_values = uploaded_df.columns)
+
+
+# then use verified field values to search for each entry in MB and Discogs
+
+@app.route('/show_entries')
+def showEntries():
+    # Retrieving uploaded file path from session
+    data_file_path = session.get('uploaded_data_file_path', None)
+
+    # read csv file in python flask (reading uploaded csv file from uploaded server location)
+    uploaded_df = pd.read_csv(data_file_path)
+
+    # return the entries found
+    # !!!! for this version we'll just return the first entry
+    mbresults = [musicbrainzngs.search_release_groups('\'"'+row[1]+'" AND "'+row[2]+'"\'') for row in uploaded_df.values]
+    dcresults = [d.search(row[2], type='release') for row in uploaded_df.values]
+
+    # grab just the first entry of each row
+    mbresult = [row['release-group-list'][0]['id'] if len(row['release-group-list']) > 0 else 'not found' for row in mbresults]
+    dcresult = [row[0].id for row in dcresults]
+
+    # each entry should be formatted clearly and have a link
+    # to its entry on MB and Discogs respectively
+    # !!!! for this version let's start with just a link
+
+    # the results should also be available as a downloadable CSV
+    # !!!! for this version let's get this going only if it feels necessary
+
+    results_list = (mbresult, dcresult)
+
+    return render_template('show_entries.html', results = results_list)
+
+
+@app.route('/find_matches')
+def findMatches():
+    # Retrieving uploaded file path from session
+    data_file_path = session.get('uploaded_data_file_path', None)
+ 
+    # read csv file in python flask (reading uploaded csv file from uploaded server location)
+    uploaded_df = pd.read_csv(data_file_path)
+
+    # get the results
+    # note: here we're using double quotes to get an EXACT match of what entered, we'll have to add some fuzziness
+    # this will also return several (looks like default 25) results according to how they match
+    # perhaps we should give the user the option to browse each result as it comes in!
+    # also depending on the columns included in the csv, we should search for each one
+    mbresults = [musicbrainzngs.search_release_groups('\'"'+row[1]+'" AND "'+row[2]+'"\'') for row in uploaded_df.values]
+    # for now let's just search using the title
+    dcresults = [d.search(row[2], type='release') for row in uploaded_df.values]
+
+    the_results = (mbresults, dcresults)
+
+    return render_template('find_matches.html', data_var = the_results)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
